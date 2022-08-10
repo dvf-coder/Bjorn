7743d760acfb4f17f74b1b4cccfc39ba53d4aa64
Bjørn <bjoern.corfitz@gmail.com>
2022-08-10 14:40:37 +0200

    change to vertical allignement

--- htmlCode.py
+++ htmlCode.py
@@ -120,9 +120,9 @@ def CodeHTML(textBlack, veganGreen, labelsKommuneList):
         ]),
         html.Div([
             dcc.RadioItems(id = 'questions',
+                           inline = True,
                           options = labelsQuestions,
                           value = questions[0],
-                          inline = True
                           )
             ])
         ],style={'background-color':'white','margin':'2%','display':'inline-block'})
