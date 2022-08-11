bc957a98ddedeaf24edf58b9332854183c15c32d
Bjørn <bjoern.corfitz@gmail.com>
2022-08-11 15:31:32 +0200

    adding layout and graphic

--- htmlCode.py
+++ htmlCode.py
@@ -180,17 +180,28 @@ def update_lollipop(value):
     [Input('questions', 'value'), Input('kommune', 'value')]
     )
 def updateRoseChart(question, kommune):
-    if question != questions[0]:
+    if question == questions[0]: # !!! remember to change to !=
         dfStorkreds = df[df['Kommune']== kommune]
         fig = px.bar_polar(
             data_frame = dfStorkreds,
             r = question,
             theta = 'Navn',
-            #color = question,
+            color = question,
             template="plotly_white",
             color_continuous_scale= px.colors.sequential.Greens,
             width=1200,
             height=720)
+        fig.update_layout(
+            #Add text to the circle (polar)
+            polar = dict(radialaxis =dict(tickvals=[0,1,2], ticktext=["<b>Uenig</b>","<b>Delvis enig</b>","<b>Enig</b>"])),
+            #Changes the font of the text
+            font=dict(family="Roboto",size=9,color="black"),
+            # Changes the colorbar
+            coloraxis_colorbar=dict(title="<b>Svarmulighed</b>",
+                                    #tickvals=[0,1,2],
+                                    ticktext=["Uenig","Delvis enig","Enig"],
+                                    lenmode="pixels", len=420)
+            )
     return fig
 
 if __name__ == '__main__':
